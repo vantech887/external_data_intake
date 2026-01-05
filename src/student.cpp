36856743ab7ab5df33041653b2e5cf29779d787b
vicary_mwamba <mwambachimwemwe887@gmail.com>
2026-01-05 10:19:50 +0200

    fully working parser working, cleaned and fixed all the previous bugs. although not fullybcapable of handling all sorts of errors as an initial build i think it works

--- src/student.cpp
+++ src/student.cpp
@@ -4,28 +4,45 @@
 #include <limits>
 #include <sstream>
 #include <vector>
-#include "student.h";
+#include "student.h"
 
 using namespace std;
 
-vector<STUDENT> studentData;
 
 vector<STUDENT> parser()
 {
+    vector<STUDENT> studentData;
     string line, xline;
+    //reading the input
     ifstream studentFile("../data/input.csv");
     if (studentFile.is_open())
     {
+        //ignoring one line
         getline(studentFile, xline);
         while(getline(studentFile, line))
         {
+            //loop that reads each line
             vector<string> tokenData;
             stringstream ss(line);
             string dataChunk;
+            //on each line read its added to the temp data vector thats used to create a single student object
             while (getline(ss, dataChunk, ','))
             {
                 tokenData.push_back(dataChunk);
             }
+            //validating that all fields are present
+            if (tokenData.size() != 4)
+            {
+                continue;
+            }
+            //creating a student object
+            STUDENT s;
+            s.ID = stoi(tokenData[0]);
+            s.name = tokenData[1];
+            s.age = stoi(tokenData[2]);
+            s.program = tokenData[3];
+            //appending it to the return vector going to the main
+            studentData.push_back(s);
             
         }
         studentFile.close();
